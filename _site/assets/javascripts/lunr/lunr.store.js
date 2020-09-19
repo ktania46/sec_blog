b7cb80c842661fbcd69416e29547f859801432f5
Tania Kapoor <tkapoor@redhat.com>
2020-09-19 20:17:16 +0530

    Add existing file

---
+++ _site/assets/javascripts/lunr/lunr.store.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "Introduction",
+        "excerpt":"Red Hat and IBM Power teams have been working together on OpenShift since 2018 with the initial release of OpenShift 3.11 on Power. Through this continuous collaboration and effort between IBM and Red Hat, improvements on Power have been made such as the support of IBM PowerVM with OpenShift to...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "http://localhost:4000/sec_blog/jekyll/update/2020/09/19/welcome-to-jekyll.html"
+      }]
